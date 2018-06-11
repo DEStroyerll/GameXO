07fcf0c38c6b5e8d6008fa6849a4082556e04034
DEStroyerll <thunderstorm.lightning@inbox.ru>
2018-06-11 23:38:33 +0300

    Сортировка чисел по возрастанию и убыванию!

---
+++ Main.java
@@ -0,0 +1,27 @@
+package GameXO;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Field field = new Field();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("СТАРТ ИГРЫ!");
+        field.displayField();
+        while (field.checkWinner() == ' ' && field.freeFields()) {
+            field.playerMove(scanner);
+            field.computerMove();
+            field.displayField();
+        }
+        if (field.checkWinner() == 'X') {
+            System.out.println("ВЫ ПОБЕДИЛИ!");
+        }
+        if (field.checkWinner() == 'O') {
+            System.out.println("ВЫ ПРОИГРАЛИ!");
+        }
+        if (field.checkWinner() == ' ' && !field.freeFields()) {
+            System.out.println("КОНЕЦ!");
+        }
+    }
+}
